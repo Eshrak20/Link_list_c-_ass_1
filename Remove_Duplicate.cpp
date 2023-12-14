e67ac5766873b6df1c57070b85b5a9e96c4f2809
Eshrak <eshrak@Eshraks-iMac-Pro.local>
2023-12-14 08:00:57 -0800

    queary done

--- Remove_Duplicate.cpp
+++ Remove_Duplicate.cpp
@@ -28,27 +28,21 @@ void Insert_value(Group *&head, Group *&tail, int value)
     tail->next = newNode;
     tail = newNode;
 };
-void delete_node(Group *start, int val)
-{
-    Group *tmp2 = start;
-    while (tmp2->next != NULL)
-    {
-        if (tmp2->next->value == val)
-        {
-            // Group *deleteNode = tmp2->next;
-            // tmp2->next = deleteNode->next;
-            // delete deleteNode;
-            cout << tmp2->next->value << " ";
-        }
-    }
-}
+
 void print_link_list(Group *head)
 {
     Group *temp = head;
     while (temp != NULL)
     {
-        delete_node(temp, temp->value);
+        cout << temp->value << " ";
         temp = temp->next;
+    }
+    cout << endl;
+    Group *temp_2 = head;
+    while (temp_2 != NULL)
+    {
+        cout << temp_2->value << " ";
+        temp_2 = temp_2->next;
     }
 };
 int main()
